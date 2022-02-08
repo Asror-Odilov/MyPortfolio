72d924891cab35707f50d0821b2de91f465d1b23
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-08 12:12:02 -0800

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import { mobile } from '../responsive'
 import Box from '@mui/material/Box';
