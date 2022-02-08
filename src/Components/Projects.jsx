488c1460185c9eb438acacdbc07f83f883551571
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-08 14:36:24 -0800

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import { mobile } from '../responsive'
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 const Projects = () => {
     useEffect(() => {
