0d8af97ef5aee06fa777a64148c372d59afdac9b
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-08 13:57:04 -0800

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -35,12 +35,13 @@ const Home = () => {
             <Container>
                 <BigCircle />
                 <Wrapper>
-                <Content  data-aos="fade-right"
-                            data-aos-offset="500"
-                            data-aos-easing="ease-in-sine">
-                    <Title>Hello!</Title>
-                    <Name><Name1>I'm</Name1> <Name2 style={{color: '#006454'}}>Asror Odilov</Name2></Name>
-                    <Text><Stick>//</Stick> Front End Developer & UI/UX designer</Text>
+                <Content>
+                    <Title data-aos="fade-right" data-aos-offset="900" data-aos-delay="700"
+                    data-aos-duration="80000">Hello!</Title>
+                    <Name data-aos="fade-right"  data-aos-delay="900" data-aos-offset="900"
+                    data-aos-duration="100000"><Name1>I'm</Name1> <Name2 style={{color: '#006454'}}>Asror Odilov</Name2></Name>
+                    <Text data-aos="fade-right" data-aos-delay="1100" data-aos-offset="900"
+                    data-aos-duration="120000"><Stick>//</Stick> Front End Developer & UI/UX designer</Text>
                     <Button onClick={handleOpen}>Resume</Button>
                 </Content>
                 <ImageContainer>
