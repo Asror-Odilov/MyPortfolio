fb0d5654d3aa46d714376ff537e6aec0c0cd71ac
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-08 14:45:52 -0800

    Update Services.jsx

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -31,7 +31,8 @@ const Services = () => {
                 </Box>
 
                 <Box data-aos="fade-up"
-                     data-aos-anchor-placement="center-bottom">
+                     data-aos-anchor-placement="center-bottom"
+                     data-aos-duration="900">
                     <Icon>
                     <SiAntdesign />
                     </Icon>
