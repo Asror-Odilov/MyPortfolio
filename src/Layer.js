8380b50359aaf4c78022a285a4e2c64d235e0a7a
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-16 12:54:41 -0800

    Create Layer.js

---
+++ src/Layer.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { Box } from './Box';
+import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
+
+
+const Layer = () => {
+    
+    useEffect(() => {
+        const cover0 = document.querySelector('.cover0');
+        
+        cover0.innerHTML = "1" ? cover0.classList.add("unvisible") : null;
+        
+    })
+
+  return (
+      <>
+    <div >
+        <div className='cover0 cover1'>1
+          
+        </div>
+        <div className='cover cover2'></div>
+        <div className='cover cover3'></div>
+          
+    </div>
+    
+    </>
+  )
+}
+
+export default Layer
+
