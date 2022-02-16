f6807b303dcb10e56ea937ef34df167f97e72b9e
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-16 12:51:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Home from './Components/Home';
 import Navigation from './Components/Navigation';
 import Projects from './Components/Projects';
 import Services from './Components/Services';
-import Layer from './Additions/Layer';
-import { Box }  from './Additions/Box';
+
+
 
 
 
 function App() {
   return (
     <div className="App">
-        <Layer />
-        <Box />
+        
+      
       <Router>
         <Navigation />
           <Home />
