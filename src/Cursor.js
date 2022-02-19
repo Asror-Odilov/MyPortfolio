adc384329b77b130ac6c7b0f8e1b390176f9ac0c
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-18 16:36:04 -0800

    Update Cursor.js

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from "styled-components";
-import { Button } from './Components/Home.jsx'
+
 const Cursor = () => {
     const [cursorX, setCursorX] = useState()
   const [cursorY, setCursorY] = useState()
