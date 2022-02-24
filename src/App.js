9416c6672f9354c2dce69acaca00ef1d683e97fd
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-23 19:27:08 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Navigation from './Components/Navigation';
 import Home from './Components/Home';
 import Skills from './Components/Skills';
 import Services from './Components/Services';
-import Projects from './Components/Projects';
+
 import Contact from './Components/Contact';
 import Layer from './Layer';
 import Cursor from './Cursor';
 import Preloader from './Preloader/Preloader';
+import Projects from './Components/Projects';
 
 
 
