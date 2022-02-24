4a0f51aed173da4f3e8a4fc90e29b62131a14e6b
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-23 18:44:42 -0800

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -15,50 +15,52 @@ const Skills = () => {
         <Line />
         <MainWrapper>
         <Wrapper>
-            <Title><span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>HTML & CSS</Title>
+            <Title data-aos="zoom-in" data-aos-duration="1000">
+                <span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>HTML & CSS</Title>
             <SkillsCon>
-                <Skill data-aos="zoom-in" data-aos-duration="1200"> 
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> 
                     <Char style={{ background: '#069e78', width: '250px' }}
                     /><Name>HTML</Name> 
                 </Skill>
-                <Skill data-aos="zoom-in" data-aos-duration="1200"> 
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> 
                     <Char style={{ background: '#069e78', width: '230px' }}
                     /><Name>CSS</Name> 
                 </Skill>
-                <Skill data-aos="zoom-in" data-aos-duration="1200"> 
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> 
                     <Char style={{ background: '#069e78', width: '210px' }}/><Name>Bootstrap</Name> 
                 </Skill>
-                <Skill data-aos="zoom-in" data-aos-duration="1200"> 
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> 
                     <Char style={{ background: '#069e78', width: '190px' }}
                     /><Name>Sass</Name> 
                 </Skill>
-                <Skill data-aos="zoom-in" data-aos-duration="1200"> 
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> 
                     <Char style={{ background: '#069e78', width: '180px' }}
                     /><Name>Styled Components</Name> 
                 </Skill>
             </SkillsCon>
         </Wrapper>
         <Wrapper>
-            <Title> <span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>JavaScript</Title>
+            <Title data-aos="zoom-in" data-aos-duration="1000"> 
+                <span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>JavaScript</Title>
             <SkillsCon>
-                <Skill> <Char style={{ background: 'teal', width: '250px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: 'teal', width: '250px' }}
                     /><Name>JavaScript</Name> </Skill>
-                <Skill> <Char style={{ background: 'teal', width: '230px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: 'teal', width: '230px' }}
                     /><Name>React.js</Name> </Skill>
-                <Skill> <Char style={{ background: 'teal', width: '200px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: 'teal', width: '200px' }}
                     /><Name>TypeScript</Name> </Skill>
             </SkillsCon>
         </Wrapper>
         <Wrapper>
-            <Title><span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>Externals</Title>
+            <Title data-aos="zoom-in" data-aos-duration="1000"><span style={{color: '#008f77', marginRight: '0.5rem',}}>//</span>Externals</Title>
             <SkillsCon>
-                <Skill> <Char style={{ background: '#0961c5', width: '250px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: '#0961c5', width: '250px' }}
                     /><Name>Material UI</Name> </Skill>
-                <Skill> <Char style={{ background: '#0961c5', width: '230px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: '#0961c5', width: '230px' }}
                     /><Name>FireBase</Name> </Skill>
-                <Skill> <Char style={{ background: '#0961c5', width: '210px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: '#0961c5', width: '210px' }}
                     /><Name>Firgma</Name> </Skill>
-                <Skill> <Char style={{ background: '#0961c5', width: '190px' }}
+                <Skill data-aos="zoom-in" data-aos-duration="1000"> <Char style={{ background: '#0961c5', width: '190px' }}
                     /><Name>Git</Name> </Skill>
                 
             </SkillsCon>
