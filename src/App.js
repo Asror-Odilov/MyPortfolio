08526fd6674e590ca6aed852621019e0b61312ea
Asror-Odilov <asrorodilov2@gmail.com>
2022-02-23 17:48:32 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './Components/Contact';
-import Home from './Components/Home';
 import Navigation from './Components/Navigation';
-import Projects from './Components/Projects';
+import Home from './Components/Home';
+import Services from './Components/Skills';
 import Services from './Components/Services';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 import Layer from './Layer';
 import Cursor from './Cursor';
 import Preloader from './Preloader/Preloader';
@@ -24,6 +25,7 @@ function App() {
         
         <Navigation />
           <Home />
+          <Skilss />
           <Services />
           <Projects />
           <Contact />  
