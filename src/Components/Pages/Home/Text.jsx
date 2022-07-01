1e2c0e7362321404be188fc70da06a6143501d05
Asror Odilov <93303891+Asror-Odilov@users.noreply.github.com>
2022-07-01 13:21:41 +0200

    Update Text.jsx

--- src/Components/Pages/Home/Text.jsx
+++ src/Components/Pages/Home/Text.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Home.css'
+import { motion } from 'framer-motion'
+
+const animation = {
+    hidden: { opacity: 0},
+    show: { opacity: 1,
+    transition: { duration: 1.3, delay: 3.7}  
+  }}
 
 const Text = () => {
   return (
-    <div className='text'>
+    <motion.div className='text'
+        variants={animation}
+        initial="hidden"
+        animate="show"
+    >
         <h1>Warning!</h1>
         <p>Portfolio and projects inside the portfolio have not finished yet or during the procces of update</p>
 
-    </div>
+    </motion.div>
   )
 }
 
